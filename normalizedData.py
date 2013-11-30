82e5fc2d7b3370b3c97e72f8e04649276fb09303
Yongfeng Zhang <yozhang@ucsc.edu>
2013-11-29 21:20:26 -0800

    Update normalizedData.py
    1.0, 2.0, 3.0, ..., 16.0 denote the attributes in the first row

--- normalizedData.py
+++ normalizedData.py
@@ -9,7 +9,7 @@ def difference(data):
     temp = np.zeros((numrows, numcolumns))
     for i in range(numrows-1):
         for j in range(numcolumns):
-            temp[i+1,j] = ((data[i+1,j] - data[i,j])/data[i,j])*100
+            temp[i+1,j] = ((float(data[i+1,j]) - float(data[i,j]))/float(data[i,j]))*100
     result = temp[1:,:]
     return result
 
@@ -23,7 +23,7 @@ def binarize(label):
     return
     
 def readdata(filename):
-    reader = np.genfromtxt(filename, dtype = float, delimiter=',')
+    reader = np.genfromtxt(filename, dtype = np.ndarray, delimiter=',')
     return reader
 
 def writetofile(filename, data):
@@ -36,7 +36,20 @@ raw_data = readdata('interpolated.csv')
 numcolumns = len(raw_data[0])
 data = raw_data[:,1:numcolumns]
 difference = difference(data) ## all values before preprocessing
-writetofile('toAnalyze.csv', difference)
-scaler = preprocessing.StandardScaler().fit(difference)
-scaler.transform(difference)    ## scaling the features to have 0 mean
-writetofile('scaled.csv', difference)
+attribute = np.array([[1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0,16.0]])
+difference = np.vstack((attribute,difference))
+##for i in range(len(difference)):
+##    if difference[i,0] >0:
+##        difference[i,0] = 1
+##    else:
+##        difference[i,0] = 0
+##writetofile('readyToLearnDiscrete.csv', difference)
+##writetofile('readyToLearn.csv', difference)
+##features = difference[:,1:]
+##label = difference[:,0]
+##scaler = preprocessing.StandardScaler().fit(features)
+##scaler.transform(features)    ## scaling the features to have 0 mean
+##binarize(label)
+##clf = svm.SVC()
+##clf.fit(features, label)
+
